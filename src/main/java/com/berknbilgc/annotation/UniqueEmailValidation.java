c9061b03482bd45a5e1697e77c6666de4cc0de50
BerkinBilgc <berknbilgc@gmail.com>
2022-11-25 16:09:38 +0300

    firstAnnotation

---
+++ src/main/java/com/berknbilgc/annotation/UniqueEmailValidation.java
@@ -0,0 +1,25 @@
+package com.berknbilgc.annotation;
+
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+//lombok
+
+
+@RequiredArgsConstructor
+public class UniqueEmailValidation implements ConstraintValidator<UserRegisterUniqueEmail,String> {
+
+    //repository
+    //private final IDailyRepository repository;
+
+    //@Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+    //    DailyEntity registerEntity=repository.findByEmail(email);
+    //    if(registerEntity!=null)
+            return false;
+    //    return true;
+    }
+
+}
